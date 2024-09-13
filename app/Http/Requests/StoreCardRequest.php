241509385702a530f2710742b7affe1255280a34
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-09-13 15:23:13 -0300

    ft: created validation for card number with algoritm luhn and for expire date

--- app/Http/Requests/StoreCardRequest.php
+++ app/Http/Requests/StoreCardRequest.php
@@ -5,22 +5,20 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Carbon\Carbon;
 
 class StoreCardRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
     /**
-    * Manipular falha de validação e retornar uma resposta JSON com os erros de validação.
-    *
-    * @param  \Illuminate\Contracts\Validation\Validator  $validator O objeto de validação que contém os erros de validação.
+    * 
+    * @param  \Illuminate\Contracts\Validation\Validator 
     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+    *
     */
     protected function failedValidation(Validator $validator)
     {
@@ -39,11 +37,66 @@ class StoreCardRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'number' => ['required', 'string', 'regex:/^\d{4} \d{4} \d{4} \d{4}$/'],
-            'expire_date' => 'required|string|regex:/^\d{2}\/\d{2}$/',
-            'CVV' => 'required|string|size:3',
+            'number' => ['required', 'string', 'regex:/^\d{4} \d{4} \d{4} \d{4}$/', function ($attribute, $value, $fail) {
+                $this->validateCardNumber($value, $fail);
+            }],
+            'expire_date' => ['required', 'string', 'regex:/^(0[1-9]|1[0-2])\/\d{2}$/', function ($attribute, $value, $fail) {
+                $this->validateFutureDate($value, $fail);
+            }],
+            'CVV' => ['required', 'string', 'regex:/^\d{3}$/'],
             'client_id' => ['nullable', 'exists:clients,id'],
         ];
+    }
+
+    /**
+     * Custom validation for card number with Algoritmo of Luhn.
+     *
+     * @param string $value
+     * @param \Closure $fail
+     */
+    private function validateCardNumber($value, $fail)
+    {
+        $cardNumber = preg_replace('/\D/', '', $value);
+        $sum = 0;
+        $shouldDouble = false;
+
+        for ($i = strlen($cardNumber) - 1; $i >= 0; $i--) {
+            $digit = $cardNumber[$i];
+
+            if ($shouldDouble) {
+                $digit *= 2;
+                if ($digit > 9) {
+                    $digit -= 9;
+                }
+            }
+
+            $sum += $digit;
+            $shouldDouble = !$shouldDouble;
+        }
+
+        if(($sum % 10) !== 0) {
+            return $fail('O número do cartão é inválido');
+        }
+    }
+
+    /**
+     * Custom validation for expire_date.
+     *
+     * @param string $value
+     * @param \Closure $fail
+     */
+    private function validateFutureDate($value, $fail)
+    {
+        [$month, $year] = explode('/', $value);
+
+        $month = (int) $month;
+        $year = (int) ('20' . $year);
+        $expirationDate = Carbon::create($year, $month, 1);
+        $nextMonth = Carbon::now()->addMonth()->startOfMonth();
+
+        if ($expirationDate->lessThan($nextMonth)) {
+            return $fail('Não é possível adicionar um cartão que está perto de vencer ou já venceu. Por favor, tente outro.');
+        }
     }
 
     public function messages(): array
@@ -51,7 +104,7 @@ class StoreCardRequest extends FormRequest
         return [
             'number.required' => 'O número do cartão é obrigatório.',
             'number.string' => 'O número do cartão deve ser uma sequência de caracteres.',
-            'number.regex' => 'O número do cartão deve estar no formato 1234 5678 9012 3456.',
+            'number.regex' => 'O número do cartão deve estar no formato 1234 5678 9012 3456. Ou seja, deve conter apenas dígitos e ter exatamente 16 dígitos.',
 
             'expire_date.required' => 'A data de expiração é obrigatória.',
             'expire_date.string' => 'A data de expiração deve ser uma sequência de caracteres.',
@@ -59,9 +112,9 @@ class StoreCardRequest extends FormRequest
 
             'CVV.required' => 'O CVV é obrigatório.',
             'CVV.string' => 'O CVV deve ser uma sequência de caracteres.',
-            'CVV.size' => 'O CVV deve ter exatamente 3 dígitos.',
+            'CVV.regex' => 'O CVV deve ter exatamente 3 dígitos no formato 137.',
 
-            'client_id.exists' => 'O client selecionado é inválido ou não existe.',
+            'client_id.exists' => 'O identificador do cliente é inválido ou não existe.',
         ];
     }
 }
--- app/Http/Requests/UpdateCardRequest.php
+++ app/Http/Requests/UpdateCardRequest.php
@@ -3,15 +3,30 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Carbon\Carbon;
 
 class UpdateCardRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
+    }
+
+    /**
+    * 
+    * @param  \Illuminate\Contracts\Validation\Validator 
+    * @throws \Illuminate\Http\Exceptions\HttpResponseException
+    *
+    */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'errors' => $validator->errors()
+            ])->setStatusCode(422)
+        );
     }
 
     /**
@@ -22,8 +37,81 @@ class UpdateCardRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'number' => ['nullable', 'string', 'regex:/^\d{4} \d{4} \d{4} \d{4}$/', function ($attribute, $value, $fail) {
+                $this->validateCardNumber($value, $fail);
+            }],
+            'expire_date' => ['nullable', 'string', 'regex:/^(0[1-9]|1[0-2])\/\d{2}$/', function ($attribute, $value, $fail) {
+                $this->validateFutureDate($value, $fail);
+            }],
+            'CVV' => ['nullable', 'string', 'regex:/^\d{3}$/'],
+            'client_id' => ['nullable', 'exists:clients,id'],
+        ];
+    }
+
+     /**
+     * Custom validation for card number with Algoritmo of Luhn.
+     *
+     * @param string $value
+     * @param \Closure $fail
+     */
+    private function validateCardNumber($value, $fail)
+    {
+        $cardNumber = preg_replace('/\D/', '', $value);
+        $sum = 0;
+        $shouldDouble = false;
+
+        for ($i = strlen($cardNumber) - 1; $i >= 0; $i--) {
+            $digit = $cardNumber[$i];
+
+            if ($shouldDouble) {
+                $digit *= 2;
+                if ($digit > 9) {
+                    $digit -= 9;
+                }
+            }
+
+            $sum += $digit;
+            $shouldDouble = !$shouldDouble;
+        }
+
+        if(($sum % 10) !== 0) {
+            return $fail('O número do cartão é inválido');
+        }
+    }
+
+    /**
+     * Custom validation for expire_date.
+     *
+     * @param string $value
+     * @param \Closure $fail
+     */
+    private function validateFutureDate($value, $fail)
+    {
+        [$month, $year] = explode('/', $value);
+
+        $month = (int) $month;
+        $year = (int) ('20' . $year);
+        $expirationDate = Carbon::create($year, $month, 1);
+        $nextMonth = Carbon::now()->addMonth()->startOfMonth();
+
+        if ($expirationDate->lessThan($nextMonth)) {
+            return $fail('Não é possível adicionar um cartão que está perto de vencer ou já venceu. Por favor, tente outro.');
+        }
+    }
+
+    public function messages(): array
+    {
+        return [
+            'number.string' => 'O número do cartão deve ser uma sequência de caracteres.',
+            'number.regex' => 'O número do cartão deve estar no formato 1234 5678 9012 3456.',
+
+            'expire_date.string' => 'A data de expiração deve ser uma sequência de caracteres.',
+            'expire_date.regex' => 'A data de expiração deve estar no formato MM/YY.',
+
+            'CVV.string' => 'O CVV deve ser uma sequência de caracteres.',
+            'CVV.size' => 'O CVV deve ter exatamente 3 dígitos.',
+
+            'client_id.exists' => 'O client selecionado é inválido ou não existe.',
         ];
     }
 }
-
