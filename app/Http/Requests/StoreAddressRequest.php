94f34a5a0d21dea3265bcabc2dd93056e4faa0a7
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-09-13 15:24:59 -0300

    fix: using quantity of max created at rule form request

--- app/Http/Requests/StoreAddressRequest.php
+++ app/Http/Requests/StoreAddressRequest.php
@@ -8,19 +8,16 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreAddressRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
     /**
-    * Manipular falha de validação e retornar uma resposta JSON com os erros de validação.
-    *
-    * @param  \Illuminate\Contracts\Validation\Validator  $validator O objeto de validação que contém os erros de validação.
+    * 
+    * @param  \Illuminate\Contracts\Validation\Validator 
     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+    *
     */
     protected function failedValidation(Validator $validator)
     {
@@ -59,28 +56,28 @@ class StoreAddressRequest extends FormRequest
         return [
             'zip_code.required' => 'O código postal (CEP) é obrigatório.',
             'zip_code.string' => 'O código postal (CEP) deve ser uma sequência de caracteres.',
-            'zip_code.max' => 'O código postal (CEP) não pode ter mais de 10 caracteres.',
+            'zip_code.max' => 'O código postal (CEP) não pode ter mais de :max caracteres.',
 
             'street.required' => 'O nome da rua é obrigatório.',
             'street.string' => 'O nome da rua deve ser uma sequência de caracteres.',
-            'street.max' => 'O nome da rua não pode ter mais de 255 caracteres.',
+            'street.max' => 'O nome da rua não pode ter mais de :max caracteres.',
 
             'additional_information.string' => 'As informações adicionais devem ser uma sequência de caracteres.',
-            'additional_information.max' => 'As informações adicionais não podem ter mais de 255 caracteres.',
+            'additional_information.max' => 'As informações adicionais não podem ter mais de :max caracteres.',
 
             'neighborhood.required' => 'O bairro é obrigatório.',
             'neighborhood.string' => 'O bairro deve ser uma sequência de caracteres.',
-            'neighborhood.max' => 'O bairro não pode ter mais de 255 caracteres.',
+            'neighborhood.max' => 'O bairro não pode ter mais de :max caracteres.',
 
             'city.required' => 'A cidade é obrigatória.',
             'city.string' => 'A cidade deve ser uma sequência de caracteres.',
-            'city.max' => 'A cidade não pode ter mais de 255 caracteres.',
+            'city.max' => 'A cidade não pode ter mais de :max caracteres.',
 
             'state.required' => 'O estado é obrigatório.',
             'state.string' => 'O estado deve ser uma sequência de caracteres.',
-            'state.max' => 'O estado não pode ter mais de 2 caracteres.',
+            'state.max' => 'O estado não pode ter mais de :max caracteres.',
 
-            'client_id.exists' => 'O client selecionado é inválido ou não existe.',
+            'client_id.exists' => 'O identificador do cliente é inválido ou não existe.',
         ];
     }
 }
