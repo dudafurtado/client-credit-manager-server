f6ec7b3d13de197233b448323bf44da803e21c1c
Maria Eduarda Furtado <furtadoduda@hotmail.com>
2024-09-15 19:31:29 -0300

    fix: took out model import unused

--- app/Http/Middleware/CheckClientOwnership.php
+++ app/Http/Middleware/CheckClientOwnership.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use App\Models\Client;
 use App\Models\Card;
 use App\Models\Address;
 
